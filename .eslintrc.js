b7f934207d2a99870956ce316618282951748106
邱举 <2201181566@qq.com>
2021-05-21 15:32:17 +0800

    update

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,7 +15,7 @@ module.exports = {
     },
     rules: {
         "prettier/prettier": ["error", {
-            "endOfLine": "crlf"
+            endOfLine: "auto"
         }],
         "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
         "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
