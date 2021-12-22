2890fc0c24d2a8e6cdb1830780a416ebde617158
real-ju <2201181566@qq.com>
2021-12-22 17:55:15 +0800

    优化vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,38 @@
-import { defineConfig } from 'vite';
+import type { UserConfig, ConfigEnv } from 'vite';
+import { loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import { resolve } from 'path';
+
+function pathResolve(dir: string) {
+  return resolve(process.cwd(), dir);
+}
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  server: {
-    // host: '0.0.0.0',
-    proxy: {
-      '/api': {
-        target: '',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
+export default ({ mode }: ConfigEnv): UserConfig => {
+  const root = process.cwd();
+  const env = loadEnv(mode, root);
+
+  return {
+    base: env.VITE_PUBLIC_PATH,
+    resolve: {
+      alias: [
+        // /@/xxxx => /src/xxxx
+        {
+          find: /@\//,
+          replacement: pathResolve('src') + '/'
+        }
+      ]
+    },
+    plugins: [vue()],
+    server: {
+      // host: '0.0.0.0',
+      proxy: {
+        '/api': {
+          target: env.VITE_DEV_SERVER_PROXY,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, '')
+        }
       }
     }
-  }
-});
+  };
+};
