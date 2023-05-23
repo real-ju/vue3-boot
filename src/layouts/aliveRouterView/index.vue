dacca342457172f2e41793277e16db94fd08ee81
real-ju <2201181566@qq.com>
2023-05-23 17:39:53 +0800

    路由缓存优化

--- src/layouts/aliveRouterView/index.vue
+++ src/layouts/aliveRouterView/index.vue
@@ -1,10 +1,24 @@
 <template>
   <router-view v-slot="{ Component, route }">
-    <component v-if="route.meta.cache === false" :is="Component" :key="route.fullPath" />
-    <keep-alive v-else>
+    <keep-alive :include="getCacheInclude(Component, route)">
       <component :is="Component" :key="route.fullPath" />
     </keep-alive>
   </router-view>
 </template>
 
-<script setup lang="ts" name="LAliveRouterView"></script>
+<script setup lang="ts" name="LAliveRouterView">
+import type { VNode } from 'vue';
+import type { RouteLocationNormalizedLoaded } from 'vue-router';
+
+const cacheInclude: Set<string> = new Set();
+const getCacheInclude = (Component: VNode, route: RouteLocationNormalizedLoaded) => {
+  // 添加路由缓存
+  if (route.meta.cache !== false) {
+    const compType: any = Component.type;
+    if (compType.name) {
+      cacheInclude.add(compType.name);
+    }
+  }
+  return Array.from(cacheInclude);
+};
+</script>
